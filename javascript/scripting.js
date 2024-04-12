44b6d104342e50142ebbec3879a58a7915bae47a
Gp32Prog66 <szojo7@hotmail.com>
2024-04-12 18:21:41 -0400

    Adding Shopping Cart Display

--- javascript/scripting.js
+++ javascript/scripting.js
@@ -8,3 +8,39 @@ function orderComplete()
     alert("Thank You! Your Order has been submitted");
 }
 
+//Checkbox Script
+function headphoneSelection()
+{
+    if(document.getElementById('selection').checked = true)
+    {
+        console.log("Checkbox has been checked");
+    }
+    else
+    {
+        alert("Box hasn't been checked");
+    }
+}
+
+function displayQuantityAndPrice(quantPrice)
+{
+    var displayBoxes = document.getElementById(quantPrice);
+
+    if(displayBoxes.visibility === "display")
+    {
+        displayBoxes.visibility = "hidden";
+    }
+    else
+    {
+        displayBoxes.visibility="display";
+    }
+}
+
+//Thanking the Customer
+const custName = document.getElementById('checkout_cust');
+
+function checkout()
+{
+    checkout_cust.innerHTML = custName.ariaValueMax;
+}
+
+orderComplete.addEventListener('click', custName);
