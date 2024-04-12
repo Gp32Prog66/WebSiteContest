937e3b8006c1b32f2b2fe45dde200c7426436ddb
Gp32Prog66 <szojo7@hotmail.com>
2024-04-12 18:23:36 -0400

    Formatted Documents

--- javascript/scripting.js
+++ javascript/scripting.js
@@ -3,43 +3,35 @@ const todaysDate = new Date();
 document.getElementById("date").innerHTML = todaysDate;
 
 //Order Completion Form
-function orderComplete()
-{
+function orderComplete() {
     alert("Thank You! Your Order has been submitted");
 }
 
 //Checkbox Script
-function headphoneSelection()
-{
-    if(document.getElementById('selection').checked = true)
-    {
+function headphoneSelection() {
+    if (document.getElementById('selection').checked = true) {
         console.log("Checkbox has been checked");
     }
-    else
-    {
+    else {
         alert("Box hasn't been checked");
     }
 }
 
-function displayQuantityAndPrice(quantPrice)
-{
+function displayQuantityAndPrice(quantPrice) {
     var displayBoxes = document.getElementById(quantPrice);
 
-    if(displayBoxes.visibility === "display")
-    {
+    if (displayBoxes.visibility === "display") {
         displayBoxes.visibility = "hidden";
     }
-    else
-    {
-        displayBoxes.visibility="display";
+    else {
+        displayBoxes.visibility = "display";
     }
 }
 
 //Thanking the Customer
 const custName = document.getElementById('checkout_cust');
 
-function checkout()
-{
+function checkout() {
     checkout_cust.innerHTML = custName.ariaValueMax;
 }
 
